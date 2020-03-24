ec71ac6472bbfa233a40f43775a5482cb727e388
Denys Klymanskyi <dr.fender17@gmail.com>
2020-03-24 18:11:20 +0200

    fast detail info

--- src/componets/FilmItem.js
+++ src/componets/FilmItem.js
@@ -1,25 +1,53 @@
 import React from 'react'
 import notFound from '../assets/img/not-found.png'
 import '../assets/style/film.scss'
+import InfoPopup from '../componets/InfoPopup'
 
-export default function (props) {
-  const link = `https://www.imdb.com/title/${props.film.imdbID}`
-  const title = props.film.Title
-  let poster = props.film.Poster
-  if (poster === 'N/A') {
-    poster = notFound
+export default class FilmItem extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      filmInfo: null,
+      isPopupOpen: false
+    }
   }
 
-  return (
-    <div className="film">
-      <div className="film__poster">
-        <img src={poster} alt="" className="film__image"/>
+  handleClick = () => {
+    fetch(`http://www.omdbapi.com/?apikey=d777cf78&i=${this.props.film.imdbID}`).then(r => r.json()).then((r) => {
+      this.setState({
+        filmInfo: r,
+        isPopupOpen: true
+      })
+    })
+  }
+
+  closePopup = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    this.setState({
+      isPopupOpen: false
+    })
+  }
+
+  render () {
+    const link = `https://www.imdb.com/title/${this.props.film.imdbID}`
+    const title = this.props.film.Title
+    let poster = this.props.film.Poster
+    if (poster === 'N/A') {
+      poster = notFound
+    }
+    return (
+      <div className="film" onClick={() => this.handleClick()}>
+        <div className="film__poster">
+          <img src={poster} alt="" className="film__image"/>
+        </div>
+        <div className="film__info">
+          {/* eslint-disable-next-line */}
+          <a className="film__title" href={link} target="_blank">{title}</a>
+        </div>
+        {this.state.isPopupOpen && <InfoPopup close={(e) => this.closePopup(e)} info={this.state.filmInfo}/>}
       </div>
-      <div className="film__info">
-        {/* eslint-disable-next-line */}
-        <a className="film__title" href={link} target="_blank">{title}</a>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
--- src/componets/InfoPopup.js
+++ src/componets/InfoPopup.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import '../assets/style/popup'
 
 export default function Popup(props) {
   return (
     <div className="popup">
       <div className="popup__content">
-        <div className="popup__close">x</div>
-        <pre>{JSON.stringify(props)}</pre>
+        <div className="popup__close" onClick={(e) => props.close(e)}>x</div>
+        <div>Title: {props.info.Title}</div>
+        <div>Actors: {props.info.Actors}</div>
+        <div>Country: {props.info.Country}</div>
+        <div>Director: {props.info.Director}</div>
+        <div>Language: {props.info.Language}</div>
+        <div>Released: {props.info.Released}</div>
+        <div>Genre: {props.info.Genre}</div>
+        <div>Type: {props.info.Type}</div>
+        <div>Year: {props.info.Year}</div>
       </div>
     </div>
   )
